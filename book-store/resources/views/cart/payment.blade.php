037c05a6774639aa3cf36788e6240fec2c0f1b2c
Luka Šulc <lukasulc0@gmail.com>
2023-05-16 22:31:57 +0200

    Create user logged in checks

--- book-store/resources/views/cart/payment.blade.php
+++ book-store/resources/views/cart/payment.blade.php
@@ -2,11 +2,11 @@
 
 @section('content')
 <div class="d-flex justify-content-center align-items-center">
-    <form action="{{ route('cart.buy', true) }}">
+    <form action="{{ route('cart.buy') }}">
         @csrf
         <button type="submit" class="btn btn-primary btn-sm">Buy</button>
     </form>
-    <form action="{{ route('cart.buy', false) }}">
+    <form action="{{ route('cart.cart') }}">
         @csrf
         <button type="submit" class="btn btn-danger btn-sm">Cancel</button>
     </form>
