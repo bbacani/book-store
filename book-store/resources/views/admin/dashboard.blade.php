5228f648c51671f6cb0778da85fabe4f081d1500
bbacani <bernard.bacani@gmail.com>
2023-05-20 21:02:57 +0200

    Add Tab navigation for lists in dashboard

--- book-store/resources/views/admin/dashboard.blade.php
+++ book-store/resources/views/admin/dashboard.blade.php
@@ -2,13 +2,25 @@
 
 @section('content')
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
+        <div class="row">
+            <div class="col-md-3">
+                <!-- Left Side Menu -->
+                <div class="list-group">
+                    <a href="#users" class="list-group-item list-group-item-action" data-bs-toggle="pill">Users</a>
+                    <a href="#orders" class="list-group-item list-group-item-action" data-bs-toggle="pill">Orders</a>
+                    <a href="#shipments" class="list-group-item list-group-item-action" data-bs-toggle="pill">Shipments</a>
+                    <a href="#books" class="list-group-item list-group-item-action" data-bs-toggle="pill">Books</a>
+                    <a href="#authors" class="list-group-item list-group-item-action" data-bs-toggle="pill">Authors</a>
+                    <a href="#categories" class="list-group-item list-group-item-action"
+                        data-bs-toggle="pill">Categories</a>
+                </div>
+            </div>
+            <div class="col-md-9">
+                <!-- Right Side Content -->
                 <div class="card">
                     <div class="card-header">
                         Admin Dashboard
                     </div>
-
                     <div class="card-body">
                         @if (session('status'))
                             <div class="alert alert-success" role="alert">
@@ -17,14 +29,28 @@
                         @endif
                     </div>
 
-                    <!-- Add Admin Dashboard Content Here -->
+                    <!-- Content Sections -->
                     <div class="card-body">
-                        @include('admin.users.list')
-                        @include('admin.orders.list')
-                        @include('admin.shipments.list')
-                        @include('admin.books.list')
-                        @include('admin.authors.list')
-                        @include('admin.categories.list')
+                        <div class="tab-content">
+                            <div class="tab-pane fade" id="users">
+                                @include('admin.users.list')
+                            </div>
+                            <div class="tab-pane fade" id="orders">
+                                @include('admin.orders.list')
+                            </div>
+                            <div class="tab-pane fade" id="shipments">
+                                @include('admin.shipments.list')
+                            </div>
+                            <div class="tab-pane fade" id="books">
+                                @include('admin.books.list')
+                            </div>
+                            <div class="tab-pane fade" id="authors">
+                                @include('admin.authors.list')
+                            </div>
+                            <div class="tab-pane fade" id="categories">
+                                @include('admin.categories.list')
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
