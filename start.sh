bbc4f5fb91681de3cd7dde31d3ab407e7c994102
bbacani <bernard.bacani@gmail.com>
2023-05-21 02:30:34 +0200

    Improve start.sh

--- start.sh
+++ start.sh
@@ -6,8 +6,15 @@ cd ..
 docker-compose build
 docker-compose up -d
 
-# TODO: Improve this!!!
-docker exec book-store-myapp-1 bash -c "php artisan migrate --force"
+# Check if the database already exists
+docker exec book-store-myapp-1 bash -c "php artisan migrate --pretend" > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+  echo "Database already exists."
+else
+  echo "Creating the database."
+  docker exec book-store-myapp-1 bash -c "php artisan migrate --force"
+fi
+
 docker exec book-store-myapp-1 bash -c "php artisan migrate:fresh --seed"
 docker exec book-store-myapp-1 bash -c "npm install"
 docker exec book-store-myapp-1 bash -c "npm run build"
